765103405396afed3265ead56292a0ee3a1dce80
RohiniKhandare <91168473+RohiniKhandare98@users.noreply.github.com>
2022-09-24 16:58:02 +0530

    Add files via upload

---
+++ src/library/management/system/LibraryManagementSystem.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package library.management.system;
+//import frames.LoginPage;
+
+
+/**
+ *
+ * @author Rohini
+ */
+public class LibraryManagementSystem {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+//        LoginPage login = new LoginPage();
+//        login.jButton3ActionPerformed();
+    }
+    
+}
